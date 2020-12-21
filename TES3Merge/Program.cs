0163a074c6a83072e96d9c613d32b5b211a0eddc
NullCascade <nullcascade@gmail.com>
2020-12-20 17:15:41 -0800

    Update version to 0.7.

--- TES3Merge/Program.cs
+++ TES3Merge/Program.cs
@@ -90,7 +90,7 @@ namespace TES3Merge
             };
             
             var version = Assembly.GetExecutingAssembly().GetName().Version;
-            Logger.WriteLine($"TES3Merge v0.6.");
+            Logger.WriteLine($"TES3Merge v0.7.");
 
             // Main execution attempt.
 #if DEBUG == false
