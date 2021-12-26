c6ed0d4bfa8ec0aa5396f29c7351c2d378951ffd
NullCascade <nullcascade@gmail.com>
2021-12-26 15:29:49 -0800

    Add utilities to edit test plugins as json.

---
+++ TES3Merge.Tests/Plugins/to_esp.bat
@@ -0,0 +1,4 @@
+tes3conv.exe -o - merge_add_effects.json merge_add_effects.esp
+tes3conv.exe -o - merge_base.json merge_base.esp
+tes3conv.exe -o - merge_edit_all.json merge_edit_all.esp
+tes3conv.exe -o - merge_minor_tweaks.json merge_minor_tweaks.esp
---
+++ TES3Merge.Tests/Plugins/to_json.bat
@@ -0,0 +1,4 @@
+tes3conv.exe -o merge_add_effects.esp merge_add_effects.json
+tes3conv.exe -o merge_base.esp merge_base.json
+tes3conv.exe -o merge_edit_all.esp merge_edit_all.json
+tes3conv.exe -o merge_minor_tweaks.esp merge_minor_tweaks.json
