c5bf87ebbcf1c5791fc83b5aa8df239944a2a823
NullCascade <nullcascade@gmail.com>
2022-01-09 14:56:47 -0800

    Start adding real plugins for merge testing.

--- TES3Merge.Tests/Plugins/to_esp.bat
+++ TES3Merge.Tests/Plugins/to_esp.bat
@@ -1,4 +1,7 @@
-tes3conv.exe -o - merge_add_effects.json merge_add_effects.esp
-tes3conv.exe -o - merge_base.json merge_base.esp
-tes3conv.exe -o - merge_edit_all.json merge_edit_all.esp
-tes3conv.exe -o - merge_minor_tweaks.json merge_minor_tweaks.esp
+tes3conv.exe -o "merge_add_effects.json" "merge_add_effects.esp"
+tes3conv.exe -o "merge_base.json" "merge_base.esp"
+tes3conv.exe -o "merge_edit_all.json" "merge_edit_all.esp"
+tes3conv.exe -o "merge_minor_tweaks.json" "merge_minor_tweaks.esp"
+tes3conv.exe -o "Morrowind.json" "Morrowind.esm"
+tes3conv.exe -o "Bob's Diverse Blood.json" "Bob's Diverse Blood.esp"
+tes3conv.exe -o "Patch for Purists.json" "Patch for Purists.esm"
--- TES3Merge.Tests/Plugins/to_json.bat
+++ TES3Merge.Tests/Plugins/to_json.bat
@@ -1,4 +1,7 @@
-tes3conv.exe -o merge_add_effects.esp merge_add_effects.json
-tes3conv.exe -o merge_base.esp merge_base.json
-tes3conv.exe -o merge_edit_all.esp merge_edit_all.json
-tes3conv.exe -o merge_minor_tweaks.esp merge_minor_tweaks.json
+tes3conv.exe -o "merge_add_effects.esp" "merge_add_effects.json"
+tes3conv.exe -o "merge_base.esp" "merge_base.json"
+tes3conv.exe -o "merge_edit_all.esp" "merge_edit_all.json"
+tes3conv.exe -o "merge_minor_tweaks.esp" "merge_minor_tweaks.json"
+tes3conv.exe -o "Morrowind.esm" "Morrowind.json"
+tes3conv.exe -o "Bob's Diverse Blood.esp" "Bob's Diverse Blood.json"
+tes3conv.exe -o "Patch for Purists.esm" "Patch for Purists.json"
