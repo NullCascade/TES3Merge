e8ff2a68be5dab1e92b9d3ef0b9947505ba93319
rfuzzo <r.fuzzo@gmail.com>
2022-04-21 01:25:06 +0200

    fix handler

--- TES3Merge/Program.cs
+++ TES3Merge/Program.cs
@@ -10,20 +10,36 @@ internal class Program
     // main entry point to parse commandline options
     private static async Task Main(string[] args)
     {
-        var inclusive = new Option<bool>(new[] { "--inclusive-list", "-i" }, "Merge lists inclusively per element (implemented for List<NPCO>)");
-        var verify = new Option<bool>(new[] { "--verify", "-v" }, "Verify esps");
+        var inclusive = new Option<bool?>(new[] { "--inclusive-list", "-i" }, "Merge lists inclusively per element (implemented for List<NPCO>)");
+        var verify = new Option<bool?>(new[] { "--verify", "-v" }, "Verify esps");
         var rootCommand = new RootCommand
         {
             inclusive,
             verify
         };
 
-        rootCommand.SetHandler((bool i) => { MergeCommand.Merge(i); }, inclusive);
-        rootCommand.SetHandler((bool v) => { VerifyCommand.Verify(v); }, verify);
+        rootCommand.SetHandler((bool? v, bool? i) => { Handler(v, i); }, verify, inclusive);
 
         var version = Assembly.GetExecutingAssembly().GetName().Version;
         Logger.WriteLine($"TES3Merge v{version}.");
 
         await rootCommand.InvokeAsync(args);
+    }
+
+    private static void Handler(bool? verify, bool? inclusive)
+    {
+        // TODO fix this
+        if (verify is not null && inclusive is null)
+        {
+            VerifyCommand.Verify(verify.Value);
+        }
+        else if (inclusive is not null && verify is null)
+        {
+            MergeCommand.Merge(inclusive.Value);
+        }
+        else
+        {
+            MergeCommand.Merge(false);
+        }
     }
 }
